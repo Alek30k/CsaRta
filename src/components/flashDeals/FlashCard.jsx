b2fec55c8ff6f0c90d8e43ce6501e000386b3a00
Alek30k <alemank23k@gmail.com>
2023-04-26 23:02:13 -0300

    last commit

--- src/components/flashDeals/FlashCard.jsx
+++ src/components/flashDeals/FlashCard.jsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 const SampleNextArrow = (props) => {
   const { onClick } = props;
@@ -38,44 +44,79 @@ const FlashCard = ({ productItems, addToCart }) => {
     prevArrow: <SamplePrevArrow />,
   };
 
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <Slider {...settings}>
         {productItems.map((productItems) => {
           return (
-            <div className="box" key={productItems}>
-              <div className="product mtop">
-                <div className="img">
-                  <span className="discount">{productItems.discount}% Off</span>
-
-                  <img src={productItems.cover} alt="" />
-
-                  <div className="product-like">
-                    <label>{count}</label> <br />
-                    <i className="fa-regular fa-heart" onClick={increment}></i>
+            <>
+              {open && (
+                <div className="slider">
+                  <FontAwesomeIcon
+                    icon={faCircleXmark}
+                    className="close"
+                    onClick={() => setOpen(false)}
+                  />
+                  <FontAwesomeIcon
+                    icon={faCircleArrowLeft}
+                    className="arrow"
+                    // onClick={() => handleMove("l")}
+                  />
+                  <div className="sliderWrapper">
+                    <img
+                      src={productItems.cover}
+                      alt=""
+                      className="sliderImg"
+                    />
                   </div>
+                  <FontAwesomeIcon
+                    icon={faCircleArrowRight}
+                    className="arrow"
+                    // onClick={() => handleMove("r")}
+                  />
                 </div>
-                <div className="product-details">
-                  <h3>{productItems.name}</h3>
-                  <div className="rate">
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
+              )}
+              <div className="box" key={productItems}>
+                <div className="product mtop">
+                  <div className="img">
+                    <span className="discount">
+                      {productItems.discount}% Off
+                    </span>
+
+                    <img src={productItems.cover} alt="" />
+
+                    <div className="product-like">
+                      <label>{count}</label> <br />
+                      <i
+                        className="fa-regular fa-heart"
+                        onClick={increment}
+                      ></i>
+                    </div>
                   </div>
-                  <div className="price">
-                    <h4>${productItems.price}.00 </h4>
-                    {/* step : 3  
+                  <div className="product-details">
+                    <h3>{productItems.name}</h3>
+                    <div className="rate">
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                    </div>
+                    <div className="price">
+                      <h4>${productItems.price}.00 </h4>
+                      {/* step : 3  
                      if hami le button ma click garryo bahne 
                     */}
-                    <button onClick={() => addToCart(productItems)}>
-                      <i className="fa fa-plus"></i>
-                    </button>
+                      <button onClick={() => addToCart(productItems)}>
+                        <i className="fa fa-plus"></i>
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </>
           );
         })}
       </Slider>
