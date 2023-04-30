71cc6fa1b8a9f18e83a52da7b5ca5088a49cd596
Alek30k <alemank23k@gmail.com>
2023-04-30 10:32:26 -0300

    last commit

--- src/components/newarrivals/Cart.jsx
+++ src/components/newarrivals/Cart.jsx
@@ -1,24 +1,28 @@
-import React from "react"
-import Ndata from "./Ndata"
+import React from "react";
+import Ndata from "./Ndata";
 
 const Cart = () => {
   return (
     <>
-      <div className='content grid product'>
+      <div className="content grid product">
         {Ndata.map((val, index) => {
           return (
-            <div className='box' key={index}>
-              <div className='img'>
-                <img src={val.cover} alt='' />
+            <div className="boxNew" key={index}>
+              <div className="img">
+                <img src={val.cover} alt="" />
               </div>
-              <h4>{val.name}</h4>
-              <span>${val.price}</span>
+              <div>
+                <h4>{val.name}</h4>
+              </div>
+              <div className="boxPrice">
+                <span>${val.price}</span>
+              </div>
             </div>
-          )
+          );
         })}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
