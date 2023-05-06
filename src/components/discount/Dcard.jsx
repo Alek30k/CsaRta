e0168835f23aa21ca34cebe6e9066162b50a3d9b
Alek30k <alemank23k@gmail.com>
2023-05-06 15:01:29 -0300

    last commit

--- src/components/discount/Dcard.jsx
+++ src/components/discount/Dcard.jsx
@@ -34,7 +34,7 @@ const Dcard = ({ addToCart }) => {
       },
       {
         breakpoint: 768,
-        settings: { slidesToShow: 3, slidesToScroll: 2, infinite: false },
+        settings: { slidesToShow: 4, slidesToScroll: 2, infinite: false },
       },
       {
         breakpoint: 991,
@@ -42,7 +42,11 @@ const Dcard = ({ addToCart }) => {
       },
       {
         breakpoint: 1024,
-        settings: { slidesToShow: 3, slidesToScroll: 3, infinite: false },
+        settings: { slidesToShow: 4, slidesToScroll: 3, infinite: false },
+      },
+      {
+        breakpoint: 1100,
+        settings: { slidesToShow: 6, slidesToScroll: 3, infinite: false },
       },
     ],
   };
