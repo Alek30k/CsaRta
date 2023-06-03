5e7199396ba4529f7c54986c15fdc5a0f5d37d40
Alek30k <alemank23k@gmail.com>
2023-06-03 18:44:00 -0300

    last commit

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,37 +2,35 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Register.css";
 import axios from "axios";
-import upload from "../../utils/upload";
+// import upload from "../../utils/upload";
+import { useDispatch } from "react-redux";
+import { loginFailure, loginStart, loginSuccess } from "../../redux/userSlice";
 
 const Register = () => {
-  const [file, setFile] = useState(null);
-  const [inputs, setInputs] = useState({
-    username: "",
-    email: "",
-    password: "",
-    passwordAgain: "",
-    img: "",
-  });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const [err, setErr] = useState(null);
 
-  const handleChange = (e) => {
-    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
-
-  const handleSubmit = async (e) => {
+  const handleSignup = async (e) => {
     e.preventDefault();
-
-    const url = await upload(file);
+    dispatch(loginStart());
     try {
-      await axios.post("https://csarta.onrender.com/api/auth/register", {
-        ...inputs,
-        img: url,
-      });
-      navigate("/login");
-    } catch (err) {
-      console.log(err);
+      const res = await axios.post(
+        "https://csarta.onrender.com/api/auth/signup",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+      dispatch(loginSuccess(res.data));
+      navigate("/");
+    } catch (error) {
+      dispatch(loginFailure());
     }
   };
 
@@ -40,46 +38,45 @@ const Register = () => {
     <div className="register">
       <div className="card">
         <div className="registerform">
-          <form onSubmit={handleSubmit}>
+          <form>
             <h1>Registrarse</h1>
             <input
               type="text"
               placeholder="Nombre"
-              name="username"
-              onChange={handleChange}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
             />
             <input
               type="email"
               placeholder="Email"
               name="email"
-              onChange={handleChange}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type="password"
               placeholder="Contraseña"
               name="password"
-              onChange={handleChange}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <input
+            {/* <input
               placeholder="Contraseña de nuevo"
               required
               type="password"
               name="passwordAgain"
               onChange={handleChange}
-            />
-            <label htmlFor="">Foto de perfil</label>
+            /> */}
+            {/* <label htmlFor="">Foto de perfil</label>
             <input
               name="img"
               type="file"
               onChange={(e) => setFile(e.target.files[0])}
-            />
+            /> */}
             {err && err}
-            <button type="submit" className="button">
+            <button type="submit" className="button" onClick={handleSignup}>
               Registrarse
             </button>
             <span>¿Tiene usted una cuenta?</span>
             <Link to="/login">
-              {/* <button >Login</button> */}
               <p>Iniciar sesión</p>
             </Link>
           </form>
