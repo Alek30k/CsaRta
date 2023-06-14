b8f78d86b0e2cd235e5c892a5ac25a719567fc64
Alek23k <alemank23k@gmail.com>
2023-06-14 16:15:56 -0300

    last commit

--- src/pages/productList/ProductListproduct.jsx
+++ src/pages/productList/ProductListproduct.jsx
@@ -148,7 +148,7 @@ const ProductListproduct = ({
             </div>
           </div>
 
-          <div className="contentWidthCat ">
+          <div className="contentWidthCat container">
             <div className="cateBanner">
               {DataCategory.map((value) => {
                 return (
