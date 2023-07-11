7058c1ffa636adbb2fbd0c392713f2328b8f25ac
Alek30k <alemank23k@gmail.com>
2023-07-10 23:08:20 -0300

    last commit

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -1,31 +1,15 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 // import logo from "../../components/assets/images/logo2.png";
+import logo from "../../components/assets/images/gifhead.gif";
 
 const Head = ({ cambiarTheme }) => {
   return (
     <>
       <section className="head ">
-        <div className="container d_flex">
-          <div className="leftHead row">
-            <i className="fa fa-phone"></i>
-            <label>+880 3456 789</label>
-          </div>
-          {/* <div className="logoHead">
-            <Link to="/">
-              <img src={logo} alt="" />
-            </Link>
-          </div> */}
-          <div className="right row RText">
-            <label>Theme FAQ"s</label>
-            {/* <input type="checkbox" onClick={cambiarTheme} /> */}
-
-            {/* <span>🏳️‍⚧️</span>
-            <label>EN</label>
-            <span>🏳️‍⚧️</span>
-            <label>USD</label> */}
-          </div>
-        </div>
+        {/* <div className="container d_flex"> */}
+        <img src={logo} alt="asd" />
+        {/* </div> */}
       </section>
     </>
   );
